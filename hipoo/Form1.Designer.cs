b18553d031cc9dbc85b213297fc017c93f77c34e
elfi <145452036+elifftorun@users.noreply.github.com>
2023-12-18 12:02:44 +0300

    Add files via upload

---
+++ hipoo/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace hipoo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblsayi2 = new System.Windows.Forms.Label();
+            this.lblsayi1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 59);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(123, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(123, 50);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // lblsayi2
+            // 
+            this.lblsayi2.AutoSize = true;
+            this.lblsayi2.Location = new System.Drawing.Point(70, 27);
+            this.lblsayi2.Name = "lblsayi2";
+            this.lblsayi2.Size = new System.Drawing.Size(39, 13);
+            this.lblsayi2.TabIndex = 4;
+            this.lblsayi2.Text = "1.Sayi:";
+            // 
+            // lblsayi1
+            // 
+            this.lblsayi1.AutoSize = true;
+            this.lblsayi1.Location = new System.Drawing.Point(70, 53);
+            this.lblsayi1.Name = "lblsayi1";
+            this.lblsayi1.Size = new System.Drawing.Size(39, 13);
+            this.lblsayi1.TabIndex = 5;
+            this.lblsayi1.Text = "2.Sayi:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(459, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(469, 59);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(130, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblsayi1);
+            this.Controls.Add(this.lblsayi2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblsayi2;
+        private System.Windows.Forms.Label lblsayi1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ hipoo/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hipoo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi1=Convert.ToInt32(textBox1.Text);
+            int sayi2=Convert.ToInt32(textBox2.Text);
+            double sonuc1 = Math.Sqrt(sayi1 * sayi1 + sayi2 * sayi2);
+            int sayi =Convert.ToInt32 (sonuc1);
+            double sonuc2 = Math.Round(Math.Sqrt(sayi1*sayi1+sayi2*sayi2),2);
+            label1.Text = sonuc2.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+          int sayi=Convert.ToInt32(textBox3.Text);
+            if (sayi < 0)
+            {
+                sayi = sayi * -1;
+            }
+            MessageBox.Show("Sayı: " + sayi);
+        }
+    }
+}
